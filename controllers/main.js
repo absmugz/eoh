e02980cd64ad036d4fc927eadd2b624244d53078
absmugz <absmugz09@gmail.com>
2017-01-17 12:09:26 +0200

    looping for species

--- controllers/main.js
+++ controllers/main.js
@@ -3,6 +3,8 @@ eohApp.controller('MainCtrl', function ($scope, $http) {
    $http.get("http://swapi.co/api/people").success(function(data) {
        $scope.people = data;
 		angular.forEach($scope.people, function(person){
+
+		//Loop through species and get species
 		console.log(person);
 	
 
