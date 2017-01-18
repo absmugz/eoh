d243695ed5f22d375db1223b41037ff024015058
absmugz <absmugz09@gmail.com>
2017-01-18 21:04:10 +0200

    loop for species and get value

--- controllers/main.js
+++ controllers/main.js
@@ -2,17 +2,29 @@
 eohApp.controller('MainCtrl', function ($scope, $http) {
    $http.get("http://swapi.co/api/people").success(function(data) {
        $scope.people = data;
-		angular.forEach($scope.people, function(person){
 
-		//Loop through species and get species
-		console.log(person);
-	
+		angular.forEach($scope.people, function(people) {
+		angular.forEach(people, function(person) {
+ 
+		$http.get(person.species).success(function(data) {
 
-		});
+		 $scope.species = data;
+		 console.log(data);
+
+
+	  	});
+
+   });
+});
+
+
+
 
 		
 
    });
 });
+
+
 
 
